033ecb7ff164f86b8fbaeeca1aefe6455e0af0ba
Sergey Kobyakov <skobyakov.std@gmail.com>
2025-01-26 00:42:12 +0500

    Add tview

--- go.mod
+++ go.mod
@@ -9,4 +9,13 @@ require github.com/redis/go-redis/v9 v9.7.0
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/tview v0.0.0-20241227133733-17b7edb88c57 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 func main() {
@@ -11,19 +14,31 @@ func main() {
 	am := NewAsynqMate("127.0.0.1:6379")
 	defer am.Close()
 
-	queues, err := am.ListAllQueuesNames(ctx)
+	list := tview.NewList()
+	res, err := am.SearchForTask(ctx, TaskStatePending, "without")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, q := range queues {
-		fmt.Println(q)
+	items, ok := res.([]interface{})
+	if !ok {
+		panic("unexpected type")
 	}
 
-	tasks, err := am.SearchForTask(ctx, TaskStatePending, "without")
-	if err != nil {
+	for _, item := range items {
+		list.AddItem(fmt.Sprintf("%v", item), "", 0, nil)
+	}
+
+	app := tview.NewApplication()
+	frame := tview.NewFrame(list).
+		SetBorders(1, 2, 2, 2, 4, 4).
+		AddText("Header left", true, tview.AlignLeft, tcell.ColorWhite).
+		AddText("Header middle", true, tview.AlignCenter, tcell.ColorWhite).
+		AddText("Header right", true, tview.AlignRight, tcell.ColorWhite).
+		AddText("Header second middle", true, tview.AlignCenter, tcell.ColorRed).
+		AddText("Footer middle", false, tview.AlignCenter, tcell.ColorGreen).
+		AddText("Footer second middle", false, tview.AlignCenter, tcell.ColorGreen)
+	if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
 		panic(err)
 	}
-
-	fmt.Println(tasks)
 }
